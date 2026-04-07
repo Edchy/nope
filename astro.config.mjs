ddadf8e9ffb4a5a68770bdf78b0c3621a56b98ba
edch <soderblomed@gmail.com>
2026-04-07 12:26:18 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,22 @@
 import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
-  // fonts: [
-  //   {
-  //     name: "Supreme",
-  //     cssVariable: "--supreme",
-  //     provider: fontProviders.fontshare(),
-  //     weights: [400, 700],
-  //     styles: ["normal"],
-  //     subsets: ["latin"],
-  //     fallbacks: ["system-ui", "sans-serif"],
-  //     optimizedFallbacks: true,
-  //   },
-  //   {
-  //     name: "Supreme",
-  //     cssVariable: "--supreme",
-  //     provider: fontProviders.fontshare(),
-  //     weights: [400],
-  //     styles: ["italic"],
-  //     subsets: ["latin"],
-  //     fallbacks: ["system-ui", "sans-serif"],
-  //     optimizedFallbacks: true,
-  //   },
-  // ],
+  fonts: [
+    {
+      name: "Jost",
+      cssVariable: "--font-jost",
+      provider: fontProviders.local(),
+      fallbacks: ["system-ui", "sans-serif"],
+      options: {
+        variants: [
+          {
+            style: "normal",
+            weight: "100 900",
+            src: ["./src/assets/fonts/Jost-VariableFont_wght.ttf"],
+            display: "swap",
+          },
+        ],
+      },
+    },
+  ],
 });
