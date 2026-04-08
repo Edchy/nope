417a40980a9a57c065140102e0ac1e6267d399a7
edch <soderblomed@gmail.com>
2026-04-08 12:43:49 +0200

    y

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,5 +18,37 @@ export default defineConfig({
         ],
       },
     },
+    {
+      name: "D-DIN",
+      cssVariable: "--font-ddin",
+      provider: fontProviders.local(),
+      fallbacks: ["system-ui", "sans-serif"],
+      options: {
+        variants: [
+          {
+            style: "normal",
+            weight: "400",
+            src: ["./src/assets/fonts/D-DIN.otf"],
+            display: "swap",
+          },
+        ],
+      },
+    },
+    {
+      name: "Metropolis",
+      cssVariable: "--font-metropolis",
+      provider: fontProviders.local(),
+      fallbacks: ["system-ui", "sans-serif"],
+      options: {
+        variants: [
+          {
+            style: "normal",
+            weight: "400",
+            src: ["./src/assets/fonts/Metropolis-Regular.woff2"],
+            display: "swap",
+          },
+        ],
+      },
+    },
   ],
 });
